68be5fcd44fc9b7d7d44d5a93008d04f7dbf87fc
JastejGrover <jastejsinghgrover@gmail.com>
2025-05-04 22:17:29 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_FemMYRqun', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '68b6d2eg3274221v2ambhveqts', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
